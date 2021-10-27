66e24bf8b0fe191c0f6723d2a134622e6f9831d2
YevhenChemesTR <yevhen.chemes@thomsonreuters.com>
2021-10-27 17:36:52 +0300

    acr name change

--- infra/main.tf
+++ infra/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "esoc-acr"
+  name                = "esocregistry"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
