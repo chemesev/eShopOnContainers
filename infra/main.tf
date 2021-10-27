b1e9878e84d0187d300759858115d46d3512a352
YevhenChemesTR <yevhen.chemes@thomsonreuters.com>
2021-10-27 17:03:28 +0300

    terraform intro

---
+++ infra/main.tf
@@ -0,0 +1,28 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "eShopOnContainers"
+  location = "westus2"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "esoc-acr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
